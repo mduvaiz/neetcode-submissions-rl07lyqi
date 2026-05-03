3e1c344d8244df50e59a9e7cb23696dc986d8351
Mohammed Uwaiz Ahmed <161956684+mduvaiz@users.noreply.github.com>
2026-05-03 10:46:05 +0100

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque <Character> stack = new ArrayDeque<>();
+        char arr[] = s.toCharArray();
+        for(char c : arr){
+            if(c=='('||c=='['||c=='{'){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                    if(c==')'&&top!='(') return false;
+                    if(c==']'&&top!='[') return false;
+                    if(c=='}'&&top!='{') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
